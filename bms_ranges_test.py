83276bf65f34692700c3173609b892fe08243cf7
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-04-28 17:35:06 +0530

    Create bms_ranges_test.py

---
+++ bms_ranges_test.py
@@ -0,0 +1,16 @@
+import unittest
+import bms_ranges_monitor
+
+class test_battery_current_ranges(unittest.TestCase):
+  def test_handled_fail_current_ranges(self):
+    self.assertTrue(bms_ranges_monitor.current_ranges([None]) == "Invalid Input")
+    
+  def test_passing_current_ranges(self):
+    self.assertTrue(bms_ranges_monitor.current_ranges([3, 3, 5, 4, 10, 11, 12]) == "Valid Input") 
+
+  def test_failing_current_ranges(self):
+    self.assertTrue(bms_ranges_monitor.current_ranges(['a']) == "Invalid Input")    
+    
+
+if __name__ == '__main__':
+  unittest.main()
