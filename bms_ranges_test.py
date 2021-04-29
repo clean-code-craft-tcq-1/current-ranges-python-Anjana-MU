861b515c30c5ad633b60ff409016b80b0efe6a39
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-04-29 10:38:25 +0530

    Update bms_ranges_test.py

--- bms_ranges_test.py
+++ bms_ranges_test.py
@@ -3,7 +3,7 @@ import bms_ranges_monitor
 
 class test_battery_current_ranges(unittest.TestCase):
   def test_handled_fail_current_ranges(self):
-    self.assertTrue(bms_ranges_monitor.current_ranges([None]) == "Invalid Input")
+    self.assertTrue(bms_ranges_monitor.current_ranges([]) == "Invalid Input")
     
   def test_passing_current_ranges(self):
     self.assertTrue(bms_ranges_monitor.current_ranges([3, 3, 5, 4, 10, 11, 12]) == "Valid Input") 
