2e3c9f26cd15b3be6d4c779d36cd59b73fb000f3
Anjana-MU <77484700+Anjana-MU@users.noreply.github.com>
2021-04-29 17:45:36 +0530

    Update bms_ranges_test.py

--- bms_ranges_test.py
+++ bms_ranges_test.py
@@ -2,14 +2,17 @@ import unittest
 import bms_ranges_monitor
 
 class test_battery_current_ranges(unittest.TestCase):
-  def test_handled_fail_current_ranges(self):
+  def test_handled_failing_current_ranges(self):
     self.assertTrue(bms_ranges_monitor.current_ranges([]) == "Invalid Input")
     
   def test_passing_current_ranges(self):
-    self.assertTrue(bms_ranges_monitor.current_ranges([3, 3, 5, 4, 10, 11, 12]) == "Valid Input") 
+    self.assertTrue(bms_ranges_monitor.current_ranges([3, 3, 4, 7 ,10, 11, 12, 13]) == "Valid Input") 
+    
+  def test_passing(self):
+    self.assertTrue(bms_ranges_monitor.current_ranges([3, 3, 4, 7]) == "Valid Input") 
 
   def test_failing_current_ranges(self):
-    self.assertTrue(bms_ranges_monitor.current_ranges(['a']) == "Invalid Input")    
+    self.assertTrue(bms_ranges_monitor.current_ranges([3,4,5,6,7,8]) == "Invalid Input")    #no continuous range,should give invalid
     
 
 if __name__ == '__main__':
